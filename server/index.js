8c9faeda1968049bd93c8e14eb765eef611d4934
Lizzietrust <ohidaelizabeth3@gmail.com>
2024-07-06 13:09:00 +0100

    started creating api routes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('Mongodb is successfully connected!');
-}).catch((err) => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongodb is successfully connected!");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
- 
+
+app.use('/api/user', userRoutes);
