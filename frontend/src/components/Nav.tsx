24d301848d62b65f365e146b977e1df5c3a3867a
Lizzietrust <ohidaelizabeth3@gmail.com>
2024-07-08 12:14:47 +0100

    created the signup page ui

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,44 +3,46 @@ import { FaSearch } from "react-icons/fa";
 
 const Nav = () => {
   return (
-    <div className="w-full h-20 bg-slate-100 z-50 fixed top-0 left-0 shadow-lg px-10 flex items-center justify-between">
-      <div className="flex items-center gap-40">
-        <Link to="/" className="text-2xl font-semibold">
-          BLOG
-        </Link>
-
-        <div className="flex items-center gap-10">
-          <Link to="/" className="font-medium text-lg text-gray-700">
-            Home
+    <div className="w-full h-20 bg-slate-100 z-50 fixed top-0 left-0 shadow-lg">
+      <div className="w-[90%] mx-auto h-full flex items-center justify-between">
+        <div className="flex items-center gap-40">
+          <Link to="/" className="text-2xl font-semibold">
+            BLOG
           </Link>
-          <Link to="/about" className="font-medium text-lg text-gray-700">
-            About
-          </Link>
-          <Link to="/projects" className="font-medium text-lg text-gray-700">
-            Projects
-          </Link>
-        </div>
-      </div>
 
-      <div className="flex items-center gap-12">
-        <div className="flex items-center justify-between p-4 w-96 h-14 rounded-full bg-gray-700">
-          <input
-            type="text"
-            className="bg-transparent outline-none border-none text-white placeholder:text-white"
-            placeholder="Type to search"
-          />
-
-          <div className="w-10 h-10 bg-slate-100 rounded-full flex items-center justify-center cursor-pointer">
-            <FaSearch />
+          <div className="flex items-center gap-10">
+            <Link to="/" className="font-medium text-lg text-gray-700">
+              Home
+            </Link>
+            <Link to="/about" className="font-medium text-lg text-gray-700">
+              About
+            </Link>
+            <Link to="/projects" className="font-medium text-lg text-gray-700">
+              Projects
+            </Link>
           </div>
         </div>
 
-        <Link
-          to="/login"
-          className="bg-transparent border border-gray-700 rounded-full w-28 h-14 flex items-center justify-center text-gray-700 font-semibold text-lg"
-        >
-          Login
-        </Link>
+        <div className="flex items-center gap-12">
+          <div className="flex items-center justify-between p-3 w-96 h-14 rounded-full bg-slate-950">
+            <input
+              type="text"
+              className="bg-transparent outline-none border-none text-white placeholder:text-white"
+              placeholder="Type to search"
+            />
+
+            <div className="w-10 h-10 bg-slate-100 rounded-full flex items-center justify-center cursor-pointer">
+              <FaSearch />
+            </div>
+          </div>
+
+          <Link
+            to="/login"
+            className="bg-transparent border border-slate-950 rounded-full w-28 h-14 flex items-center justify-center text-slate-950 font-semibold text-lg"
+          >
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,74 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div
+      className="pt-28 pb-6 min-h-screen w-full flex items-center justify-center"
+      style={{
+        background: `url('/bg-image.jpg') no-repeat center center / cover`,
+      }}
+    >
+      <div className="glass-card w-[500px] h-[550px] border border-slate-200 p-5 rounded-lg">
+        <h2 className="uppercase font-semibold text-4xl text-center text-white pb-10">
+          sign up
+        </h2>
+        <form className="w-full flex flex-col gap-5 text-white">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="username" className="uppercase">
+              Username
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="w-full h-14 rounded-full bg-slate-950 p-4 placeholder:text-white"
+              placeholder="Enter Username"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="uppercase">
+              Email
+            </label>
+            <input
+              type="email"
+              name=""
+              id=""
+              className="w-full h-14 rounded-full bg-slate-950 p-4 placeholder:text-white"
+              placeholder="Enter Email"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="uppercase">
+              Password
+            </label>
+            <input
+              type="password"
+              name=""
+              id=""
+              className="w-full h-14 rounded-full bg-slate-950 p-4 placeholder:text-white"
+              placeholder="Enter Password"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full h-14 rounded-full bg-transparent border border-slate-950 uppercase"
+          >
+            submit
+          </button>
+        </form>
+
+        <div className="text-center text-white pt-5">
+          Already have an account?
+          <Link to="/login" className="text-slate-950 font-semibold pl-2">
+            Log in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
