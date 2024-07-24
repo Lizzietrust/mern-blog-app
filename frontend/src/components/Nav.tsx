27ea63f6e47eae8ebfb28ba802256952359d7eaa
Lizzietrust <ohidaelizabeth3@gmail.com>
2024-07-24 16:13:45 +0100

    added authenticated user details to navigation bar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,17 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { DiVim } from "react-icons/di";
 
 const Nav = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown((prevState) => !prevState);
+  };
+
   return (
     <div className="w-full h-20 bg-slate-100 z-50 fixed top-0 left-0 shadow-lg">
       <div className="w-[90%] mx-auto h-full flex items-center justify-between">
@@ -20,6 +30,9 @@ const Nav = () => {
             <Link to="/projects" className="font-medium text-lg text-gray-700">
               Projects
             </Link>
+            <Link to="/dashboard" className="font-medium text-lg text-gray-700">
+              Dashboard
+            </Link>
           </div>
         </div>
 
@@ -36,14 +49,46 @@ const Nav = () => {
             </div>
           </div>
 
-          <Link
-            to="/login"
-            className="bg-transparent border border-slate-950 rounded-full w-28 h-14 flex items-center justify-center text-slate-950 font-semibold text-lg"
-          >
-            Login
-          </Link>
+          {currentUser ? (
+            <div className="cursor-pointer" onClick={toggleDropdown}>
+              <img
+                src={currentUser?.profilePicture}
+                alt=""
+                className="w-12 h-12 rounded-full object-cover"
+              />
+            </div>
+          ) : (
+            <Link
+              to="/login"
+              className="bg-transparent border border-slate-950 rounded-full w-28 h-14 flex items-center justify-center text-slate-950 font-semibold text-lg"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </div>
+      {dropdown && (
+        <div className="bg-slate-950 w-[220px] rounded-xl absolute top-20 right-[5%] p-4">
+          <div className="pb-5">
+            <p className="text-white capitalize font-medium text-sm">
+              {currentUser?.username}
+            </p>
+            <p className="text-slate-300 capitalize font-medium text-xs truncate w-[80%]">
+              {currentUser?.email}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className=" hover:bg-slate-900 py-2">
+              <Link to="/dashboard?tab=profile" className="text-white">
+                Profile
+              </Link>
+            </div>
+            <div className=" hover:bg-slate-900 py-2">
+              <button className="text-white">Sign out</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
