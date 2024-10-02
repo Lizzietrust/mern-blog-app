5546c9972a0589f6cc7f38830deebea0840baf93
Lizzietrust <ohidaelizabeth3@gmail.com>
2024-10-02 04:49:11 +0100

    created endpoint for deleting user

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { test, updateUser, deleteUser } from "../controllers/user.controller.js";
 import { authUser } from "../utils/authUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", authUser, updateUser);
+router.delete("/delete/:userId", authUser, deleteUser);
 
 export default router;
